b9c891cfd567280e8b5e15eac3341e9916410631
prio101 <miprio101@gmail.com>
2025-04-06 09:01:58 +0600

    Feat: Search Input

--- src/main.js
+++ src/main.js
@@ -2,15 +2,22 @@ import './style.css'
 
 import FetchAllBooks from './gutendex/fetchAllBooks';
 import SearchBook from './gutendex/searchBook';
+
+
 const appElement = document.querySelector('#app');
 const searchInput = document.querySelector('#search');
+
+
 const fetchAllBooks = new FetchAllBooks();
 const searchBook = new SearchBook()
+
+// user defined settings
+let searchTimeout;
+
 let booksResult = await fetchAllBooks.getAllBooks();
-
-
 if (booksResult.length > 0) {
   appElement.innerHTML = booksResult.map(book => `
+    <h2 id="total-book-result"></h2>
     <div class="card">
       <h2>${book.title}</h2>
 
@@ -37,37 +44,40 @@ searchInput.innerHTML = `
          id="search-input"
          placeholder="Search by title..."
          class="search-bar" />
-  <button id="search-button">Search</button>
 `;
 
 
-const searchButton = document.querySelector('#search-button');
 const searchInputField = document.querySelector('#search-input');
-searchButton.addEventListener('click', async () => {
-  const searchTerm = searchInputField.value;
-  if (searchTerm) {
-    const filteredBooks = await searchBook.fetchBooksByTitle(searchTerm).then(data => {
-      appElement.innerHTML = data.results.map(book => `
-        <div class="card">
-          <h2>${book.title}</h2>
+searchInputField.addEventListener('input', () => {
+  clearTimeout(searchTimeout);
+  searchTimeout = setTimeout(async () => {
+    console.log('Input event triggered', searchInputField.value);
+    const searchTerm = searchInputField.value;
+    if (searchTerm) {
+      await searchBook.fetchBooksByTitle(searchTerm).then(data => {
+        appElement.innerHTML = data.results.map(book => `
+          <div class="card">
 
-          <div class="image-container">
-            <img src="${book.formats['image/jpeg']}" alt="${book.title}" />
-          </div>
-          <div class="author-container">
-            <p>Authors: ${book.authors.map(author => author.name).join(', ')}</p>
-          </div>
+            <h2>${book.title}</h2>
 
-          <div class="id">
-            <p>ID: ${book.id}</p>
-          </div>
+            <div class="image-container">
+              <img src="${book.formats['image/jpeg']}" alt="${book.title}" />
+            </div>
+            <div class="author-container">
+              <p>Authors: ${book.authors.map(author => author.name).join(', ')}</p>
+            </div>
 
-          <a>WishList</a>
-        </div>
-      `).join('');
-      if (data.results.length === 0) {
-        appElement.innerHTML = '<p>No results found.</p>';
-      }
-    });
-  }
+            <div class="id">
+              <p>ID: ${book.id}</p>
+            </div>
+
+            <a>WishList</a>
+          </div>
+        `).join('');
+        if (data.results.length === 0) {
+          appElement.innerHTML = '<p>No results found.</p>';
+        }
+      });
+    }
+  }, 1000); // Delay of 1000ms after the user stops typing
 });
